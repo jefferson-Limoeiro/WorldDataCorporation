8c7e842123cf111870b5ff68e95a79e648f61d54
jefferson <jefferson.douglas.sousa07@aluno.ifce.edu.br>
2023-03-15 11:28:07 -0300

    primeiro commit do projeto lp2

---
+++ src/main/java/com/wolrd/WorldDataCorporation/WorldDataCorporationApplication.java
@@ -0,0 +1,13 @@
+package com.wolrd.WorldDataCorporation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldDataCorporationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldDataCorporationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wolrd/WorldDataCorporation/WorldDataCorporationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolrd.WorldDataCorporation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldDataCorporationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
